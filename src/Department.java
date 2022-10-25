66ac203063f585f821a1f555c61b3454d2f98332
josh <joshuajohngeorge@yahoo.co.uk>
2022-10-25 14:18:50 +0100

    Add Manager class and department

---
+++ src/Department.java
@@ -0,0 +1,5 @@
+public enum Department {
+    BUSINESS, TECHNOLOGY, SUPPORT, OPERATIONS, RESOURCES, SALES
+
+
+}
---
+++ src/Manager.java
@@ -0,0 +1,39 @@
+import java.util.Date;
+
+public class Manager extends Person{
+
+    private int ManagerId;
+    private Department department;
+    private String company;
+
+
+    public Manager(int managerId, String company) {
+        if(company==null) throw new IllegalArgumentException("Please provide the company.");
+        ManagerId = managerId;
+
+
+    }
+
+    public Manager(String FirstName, String LastName, Date BirthDate, String Nationality, int managerId, Department department, String company, String gender) {
+        super(FirstName, LastName, BirthDate, Nationality, true, gender);
+        if(company==null) throw new IllegalArgumentException("Please provide the company.");
+        this.department= department;
+
+        setManagerId(managerId);
+    }
+
+    public int getManagerId() {
+        return ManagerId;
+    }
+
+    public void setManagerId(int managerId) {
+        ManagerId = managerId;
+    }
+
+    @Override
+    public String toString() {
+        return super.getFirstName() +" " + super.getLastName()+ ", who was born on "+ super.getBirthDate()+ " is currently a manager."+
+                (super.getGender().toLowerCase()=="male"?"He":"She")+ " is from " + super.getNationality()+"."
+                ;
+    }
+}
