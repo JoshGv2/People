6552603d593dfc8e33784d6df560a999bfa8d780
Karim-atab <atabkarim@gmail.com>
2022-10-25 13:56:10 +0100

    Added toString and constructor to person class

--- src/Person.java
+++ src/Person.java
@@ -5,4 +5,21 @@ public class Person {
     private String LastName;
     private Date BirthDate;
     private String Nationality;
+
+
+    public String toString(){
+        return "The persons name is " + this.FirstName +" "+this.LastName +
+                ". They were born on " + this.BirthDate + " and they are from "+this.Nationality;
+    }
+    public Person(){
+
+    };
+    public Person (String FirstName, String LastName, Date BirthDate, String Nationality){
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.BirthDate = BirthDate;
+        this.Nationality = Nationality;
+    }
 }
+
+
